b6b3e225e565c8e8f64ac3b4dcb0ca2b96a4c890
L <sebasgarzons88@gmail.com>
2022-08-23 00:43:06 -0400

    Fixed Documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+""" Return a Tuple """
 
 from typing import Tuple
+
+
 def index_range(page: int, page_size: int) -> Tuple:
-    return((page - 1) * page_size, page * page_size)
+    return ((page - 1) * page_size, page * page_size)

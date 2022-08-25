0c35f006d1b06db9c4c8093f08d6ae423b9b32da
L <sebasgarzons88@gmail.com>
2022-08-24 23:54:03 -0400

    Basic Dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

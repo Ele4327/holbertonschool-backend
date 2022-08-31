301e416c1911b069112ff102eebb055efcca44e2
L <sebasgarzons88@gmail.com>
2022-08-30 23:50:50 -0400

    Fourth Task

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+    Instantiate the Babel object in your app.
+    Store it in a module-level variable
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+        In order to configure available languages in our app,
+        you will create a Config class that has a
+        LANGUAGES class attribute equal to ["en", "fr"].
+
+        Use Config to set Babel’s default locale
+        ("en") and timezone ("UTC").
+
+        Use that class as config for your Flask app.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object('4-app.Config')
+
+
+@app.route('/')
+def index():
+    """ Route to index.html """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+        Function with the babel.localeselector decorator.
+        Use request.accept_languages to determine
+        the best match with our supported languages.
+    """
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

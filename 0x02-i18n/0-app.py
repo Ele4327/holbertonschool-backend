555158d07d968738028665bd21e4cfbf488bf364
L <sebasgarzons88@gmail.com>
2022-08-30 22:52:15 -0400

    Zero Task

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Setup a basick flask app
+"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'], strict_slashes=False)
+def index():
+    """ Route to index.html"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
